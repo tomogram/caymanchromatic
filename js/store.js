90cc954f5694aaa504b3b1dd1684616967881478
tomogram <65041627+tomogram@users.noreply.github.com>
2020-05-22 14:52:41 -0500

    Tweaks (200522)

---
+++ js/store.js
@@ -0,0 +1,10 @@
+//Swiper Script
+
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
