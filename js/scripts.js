6b301e2514a1666928bfe4de8351266afd0b4c1e
tomogram <65041627+tomogram@users.noreply.github.com>
2020-07-13 15:27:11 -0500

    Hover Gallery btn
    Hover Gallery btn plus some new CC3 art works

--- js/scripts.js
+++ js/scripts.js
@@ -44,25 +44,35 @@ icons.forEach (icon => {
 
 //Topbar Dropdown
 
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
+$(document).ready(function () {
+  $(".dropdown").hover(
+function () {
+   $('.dropdown-content').addClass('show');
+}, 
+function () {
+   $('.dropdown-content').removeClass('show');
 }
+);
+
+});
+
+// function myFunction() {
+//   document.getElementById("myDropdown").classList.toggle("show");
+// }
 
 // Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.gal-btn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
+// window.onclick = function(event) {
+//   if (!event.target.matches('.gal-btn')) {
+//     var dropdowns = document.getElementsByClassName("dropdown-content");
+//     var i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains('show')) {
+//         openDropdown.classList.remove('show');
+//       }
+//     }
+//   }
+// }
 
 $(document).ready(function(){
   // Add smooth scrolling to all links
